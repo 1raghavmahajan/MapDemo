9fad20536cb0fa8898677836b342c79e4b22fbab
Roger <raghav.intrigue@gmail.com>
2018-06-08 17:55:01 +0530

    -Updated Dependencies

--- Application/src/main/java/com/example/locolhive/map/mapdemo/MainActivity.java
+++ Application/src/main/java/com/example/locolhive/map/mapdemo/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -28,134 +27,133 @@ import com.google.android.gms.maps.model.LatLngBounds;
 
 public class MainActivity extends AppCompatActivity implements PlaceSelectionListener {
 
-    private static String TAG = MainActivity.class.getSimpleName() + "YOYO";
+  private static String TAG = MainActivity.class.getSimpleName() + "YOYO";
 
-    private TextView mPlaceDetailsText;
-    private TextView mPlaceAttribution;
-    private static final int REQUEST_PLACE_PICKER = 1;
-    Place placeSelected;
-    Button btnViewRegion;
+  private TextView mPlaceDetailsText;
+  private TextView mPlaceAttribution;
+  private static final int REQUEST_PLACE_PICKER = 1;
+  Place placeSelected;
+  Button btnViewRegion;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_main);
 
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-                getFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+    PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+        getFragmentManager().findFragmentById(R.id.autocomplete_fragment);
 
-        AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
-                .setCountry("IN")
-                .build();
+    AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+        .setCountry("IN")
+        .build();
 
-        autocompleteFragment.setOnPlaceSelectedListener(this);
-        autocompleteFragment.setFilter(typeFilter);
+    autocompleteFragment.setOnPlaceSelectedListener(this);
+    autocompleteFragment.setFilter(typeFilter);
 
-        mPlaceDetailsText = findViewById(R.id.place_details);
-        mPlaceAttribution = findViewById(R.id.place_attribution);
-        btnViewRegion  = findViewById(R.id.btn_viewRegion);
+    mPlaceDetailsText = findViewById(R.id.place_details);
+    mPlaceAttribution = findViewById(R.id.place_attribution);
+    btnViewRegion = findViewById(R.id.btn_viewRegion);
 
-        btnViewRegion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(placeSelected!=null){
-                    if(placeSelected.isDataValid()){
-                        Intent i = new Intent(MainActivity.this,MapActivity.class);
-                        i.putExtra("Lat",placeSelected.getLatLng().latitude);
-                        i.putExtra("Lng",placeSelected.getLatLng().longitude);
-                        startActivity(i);
-                    }
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onPlaceSelected(Place place) {
-        Log.i(TAG, "Place Selected: " + place.getName());
-        LatLng latLng = place.getLatLng();
-        Log.i(TAG, "Latlong: " + latLng.toString());
-
-        LatLng latLng1 = new LatLng(latLng.latitude-0.01, latLng.longitude);
-        LatLng latLng2 = new LatLng(latLng.latitude+0.01, latLng.longitude);
-        LatLngBounds latLngBounds =
-                LatLngBounds.builder()
-                        .include(latLng)
-                        .include(latLng1)
-                        .include(latLng2).build();
-
-        try {
-            PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder().setLatLngBounds(latLngBounds);
-            Intent intent = intentBuilder.build(this);
-            // Start the Intent by requesting a result, identified by a request code.
-            startActivityForResult(intent, REQUEST_PLACE_PICKER);
-
-        } catch (GooglePlayServicesRepairableException e) {
-            GooglePlayServicesUtil
-                    .getErrorDialog(e.getConnectionStatusCode(), this, 0);
-        } catch (GooglePlayServicesNotAvailableException e) {
-            Toast.makeText(this, "Google Play Services is not available.",
-                    Toast.LENGTH_LONG)
-                    .show();
+    btnViewRegion.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (placeSelected != null) {
+          if (placeSelected.isDataValid()) {
+            Intent i = new Intent(MainActivity.this, MapActivity.class);
+            i.putExtra("Lat", placeSelected.getLatLng().latitude);
+            i.putExtra("Lng", placeSelected.getLatLng().longitude);
+            startActivity(i);
+          }
         }
+      }
+    });
+  }
 
+  @Override
+  public void onPlaceSelected(Place place) {
+    Log.i(TAG, "Place Selected: " + place.getName());
+    LatLng latLng = place.getLatLng();
+    Log.i(TAG, "Latlong: " + latLng.toString());
+
+    LatLng latLng1 = new LatLng(latLng.latitude - 0.01, latLng.longitude);
+    LatLng latLng2 = new LatLng(latLng.latitude + 0.01, latLng.longitude);
+    LatLngBounds latLngBounds =
+        LatLngBounds.builder()
+            .include(latLng)
+            .include(latLng1)
+            .include(latLng2).build();
+
+    try {
+      PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder().setLatLngBounds(latLngBounds);
+      Intent intent = intentBuilder.build(this);
+
+      startActivityForResult(intent, REQUEST_PLACE_PICKER);
+
+    } catch (GooglePlayServicesRepairableException e) {
+      GooglePlayServicesUtil
+          .getErrorDialog(e.getConnectionStatusCode(), this, 0);
+    } catch (GooglePlayServicesNotAvailableException e) {
+      Toast.makeText(this, "Google Play Services is not available.",
+          Toast.LENGTH_LONG)
+          .show();
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // BEGIN_INCLUDE(activity_result)
-        if (requestCode == REQUEST_PLACE_PICKER) {
-            // This result is from the PlacePicker dialog.
+  }
 
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    // BEGIN_INCLUDE(activity_result)
+    if (requestCode == REQUEST_PLACE_PICKER) {
+      // This result is from the PlacePicker dialog.
 
-            if (resultCode == AppCompatActivity.RESULT_OK) {
+      if (resultCode == AppCompatActivity.RESULT_OK) {
                 /* User has picked a place, extract data.
                    Data is extracted from the returned intent by retrieving a Place object from
                    the PlacePicker.
                  */
-                final Place place = PlacePicker.getPlace(data, this);
-                placeSelected = place;
-                btnViewRegion.setEnabled(true);
-                ///         Format the returned place's details and display them in the TextView.
-                mPlaceDetailsText.setText(formatPlaceDetails(getResources(), place.getName(), place.getId(),
-                        place.getAddress(), place.getPhoneNumber(), place.getWebsiteUri()));
+        final Place place = PlacePicker.getPlace(data, this);
+        placeSelected = place;
+        btnViewRegion.setEnabled(true);
+        ///         Format the returned place's details and display them in the TextView.
+        mPlaceDetailsText.setText(formatPlaceDetails(getResources(), place.getName(), place.getId(),
+            place.getAddress(), place.getPhoneNumber(), place.getWebsiteUri()));
 
-                CharSequence attributions = place.getAttributions();
-                if (!TextUtils.isEmpty(attributions)) {
-                    mPlaceAttribution.setText(Html.fromHtml(attributions.toString()));
-                } else {
-                    mPlaceAttribution.setText("");
-                }
-
-            } else {
-                // User has not selected a place, hide the card.
-                placeSelected = null;
-                btnViewRegion.setEnabled(false);
-               Log.e(TAG, "No place Selected");
-            }
-
+        CharSequence attributions = place.getAttributions();
+        if (!TextUtils.isEmpty(attributions)) {
+          mPlaceAttribution.setText(Html.fromHtml(attributions.toString()));
         } else {
-            super.onActivityResult(requestCode, resultCode, data);
+          mPlaceAttribution.setText("");
         }
-        // END_INCLUDE(activity_result)
+
+      } else {
+        // User has not selected a place, hide the card.
+        placeSelected = null;
+        btnViewRegion.setEnabled(false);
+        Log.e(TAG, "No place Selected");
+      }
+
+    } else {
+      super.onActivityResult(requestCode, resultCode, data);
     }
+    // END_INCLUDE(activity_result)
+  }
 
-    @Override
-    public void onError(Status status) {
-        Log.e(TAG, "onError: Status = " + status.toString());
+  @Override
+  public void onError(Status status) {
+    Log.e(TAG, "onError: Status = " + status.toString());
 
-        Toast.makeText(this, "Place selection failed: " + status.getStatusMessage(),
-                Toast.LENGTH_SHORT).show();
-    }
+    Toast.makeText(this, "Place selection failed: " + status.getStatusMessage(),
+        Toast.LENGTH_SHORT).show();
+  }
 
-    private static Spanned formatPlaceDetails(Resources res, CharSequence name, String id,
-            CharSequence address, CharSequence phoneNumber, Uri websiteUri) {
-        Log.e(TAG, res.getString(R.string.place_details, name, id, address, phoneNumber,
-                websiteUri));
-        return Html.fromHtml(res.getString(R.string.place_details, name, id, address, phoneNumber,
-                websiteUri));
+  private static Spanned formatPlaceDetails(Resources res, CharSequence name, String id,
+      CharSequence address, CharSequence phoneNumber, Uri websiteUri) {
+    Log.e(TAG, res.getString(R.string.place_details, name, id, address, phoneNumber,
+        websiteUri));
+    return Html.fromHtml(res.getString(R.string.place_details, name, id, address, phoneNumber,
+        websiteUri));
 
-    }
+  }
 
 }
